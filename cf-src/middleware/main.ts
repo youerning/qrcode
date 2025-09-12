2dee01f6e758e60c711f915d3f360ef62f4b5a61
youerning <673125641@qq.com>
2025-09-12 19:59:25 +0800

    feat: 更新ms clarity代码

--- cf-src/middleware/main.ts
+++ cf-src/middleware/main.ts
@@ -30,7 +30,7 @@ const gaScript = `<!-- Google tag (gtag.js) -->
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "sf1930vsh8");
+    })(window, document, "clarity", "script", "t9ldgc423t");
 </script>
 `;
 
